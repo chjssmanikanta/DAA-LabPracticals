1e28d02068806429713521de249e3d4f95623733
chjssmanikanta <chjssmanikanta@gmail.com>
2026-08-12 23:49:29 +0530

    Update Max_heap..py

--- Pratical3/Max_heap..py
+++ Pratical3/Max_heap..py
@@ -1,4 +1,3 @@
-# Function to maintain Max Heap property
 def heapify(heap, n, i):
     largest = i
     left = 2 * i + 1
@@ -14,15 +13,9 @@ def heapify(heap, n, i):
         heap[i], heap[largest] = heap[largest], heap[i]
 
         heapify(heap, n, largest)
-
-
-# Function to build Max Heap
 def build_max_heap(heap, n):
     for i in range(n // 2 - 1, -1, -1):
         heapify(heap, n, i)
-
-
-# Main program
 n = int(input("Enter the number of elements: "))
 
 heap = []
@@ -31,11 +24,7 @@ print("Enter", n, "elements:")
 
 for i in range(n):
     heap.append(int(input()))
-
-
 print("\nInput Array:", *heap)
-
-# Build Max Heap
 build_max_heap(heap, n)
 
 print("\nMax Heap:", *heap)
