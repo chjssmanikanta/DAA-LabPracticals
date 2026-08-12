0e0b99e3055ad96a1494c44c10dfe083d18a5d48
chjssmanikanta <chjssmanikanta@gmail.com>
2026-08-12 23:23:09 +0530

    Create Min-heap.py

---
+++ Pratical3/Min-heap.py
@@ -0,0 +1,43 @@
+# Function to maintain Min Heap property
+def heapify(heap, n, i):
+    smallest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and heap[left] < heap[smallest]:
+        smallest = left
+
+    if right < n and heap[right] < heap[smallest]:
+        smallest = right
+
+    if smallest != i:
+        heap[i], heap[smallest] = heap[smallest], heap[i]
+
+        heapify(heap, n, smallest)
+
+
+# Function to build Min Heap
+def build_min_heap(heap, n):
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(heap, n, i)
+
+
+# Main program
+n = int(input("Enter the number of elements: "))
+
+heap = []
+
+print("Enter", n, "elements:")
+
+for i in range(n):
+    heap.append(int(input()))
+
+
+print("\nInput Array:", *heap)
+
+# Build Min Heap
+build_min_heap(heap, n)
+
+print("\nMin Heap:", *heap)
+
+print("Minimum Element:", heap[0])
